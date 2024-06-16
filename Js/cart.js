f7e0e77d02579c32992dd50e94f86ebebce70967
Rudecindoroca <143553757+Rudecindoroca@users.noreply.github.com>
2024-06-16 20:08:16 -0300

    Terminado el carrito, falta el boton de finalizar compra. Se guardan los productos en el LS y se muestran en pantalla

---
+++ Js/cart.js
@@ -0,0 +1,62 @@
+// recipero localstorage 
+
+let recuperoStorage = localStorage.getItem(`cartItems`);
+console.log("localstorage: ", localStorage)
+let mensaje = "Tu carrito esta vacio";
+let lista = document.querySelector(`.Productos`)
+let elementosCarrito = "";
+
+
+//Veo si hay algo en el storage
+if(recuperoStorage == null){
+    // Hago parse
+    
+    let empty = document.querySelector(`.empty`);
+    empty.innerText = mensaje;
+
+} else {
+    carrito =[];
+    carrito = JSON.parse(recuperoStorage);
+    for (let i = 0; i <carrito.length; i++){
+        const productoId = carrito[i];
+        const url = `https://fakestoreapi.com/products/${productoId}`;
+        //fetch 
+        fetch(url)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+           
+            console.log(data);
+            elementosCarrito += `<div class ="productov">
+                                    <img src=${data.image} alt='${data.title}'>
+                                    <p>Nombre : ${data.title}</p>
+                                    <p>Price: $${data.price}</p>
+                
+                                </div>`;
+
+                                lista.innerHTML = elementosCarrito;
+
+
+        })
+        .catch(function(e){
+            console.log(e);
+        })
+        
+      
+
+
+
+
+
+
+
+
+
+        
+    }
+
+
+}
+
+
